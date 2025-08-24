d99c02bf8f4d8b5a0eb830e2b258eadfa93b65bc
Ishant Sahu <ishantsahu@ninjacart.com>
2025-08-24 12:19:06 +0530

    initial commit

---
+++ packages/container/config/webpack.common.js
@@ -0,0 +1,16 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/, // handles both .js and .jsx
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
